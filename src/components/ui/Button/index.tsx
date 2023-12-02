dcbcbce127cb654337f90432a84bcb5c2aaafc93
Oluwaferanmi Adeniji <feranmidev@gmail.com>
2023-12-02 12:10:23 +0100

    Button component

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -1,6 +1,6 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import { forwardRef, type ComponentProps } from "react";
-import { cn } from "../../utils";
+import { cn } from "../../../utils";
 
 const buttonStyles = cva(
   [
