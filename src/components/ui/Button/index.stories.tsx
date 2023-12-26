dcbe6e49f2bc54045decaf999303dabc3ddd38e2
Oluwaferanmi Adeniji <feranmidev@gmail.com>
2023-12-26 14:30:00 +0100

    Button Component

--- src/components/ui/Button/index.stories.tsx
+++ src/components/ui/Button/index.stories.tsx
@@ -1,30 +1,205 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from ".";
 
+// Plus icon component for examples
+const PlusIcon = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+  >
+    <path d="M12 5v14M5 12h14" />
+  </svg>
+);
+
 const meta: Meta<typeof Button> = {
   title: "Components/Button",
   component: Button,
   tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      control: { type: "select" },
+      options: [
+        "primary",
+        "secondary",
+        "danger",
+        "success",
+        "warning",
+        "dark",
+        "outline",
+        "ghost",
+      ],
+    },
+    size: {
+      control: { type: "select" },
+      options: ["sm", "md", "lg"],
+    },
+    iconPosition: {
+      control: { type: "select" },
+      options: ["left", "right"],
+    },
+    disabled: {
+      control: { type: "boolean" },
+    },
+  },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Solid: Story = {
+export const Primary: Story = {
   args: {
-    children: "Solid Button",
-    variant: "solid",
+    children: "New Transaction",
+    variant: "primary",
     size: "md",
-    colorscheme: "primary",
+    icon: <PlusIcon />,
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    children: "New Transaction",
+    variant: "secondary",
+    size: "md",
+    icon: <PlusIcon />,
+  },
+};
+
+export const Danger: Story = {
+  args: {
+    children: "Delete Item",
+    variant: "danger",
+    size: "md",
+    icon: <PlusIcon />,
+  },
+};
+
+export const Success: Story = {
+  args: {
+    children: "Save Changes",
+    variant: "success",
+    size: "md",
+    icon: <PlusIcon />,
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    children: "New Transaction",
+    variant: "warning",
+    size: "md",
+    icon: <PlusIcon />,
+  },
+};
+
+export const Dark: Story = {
+  args: {
+    children: "New Transaction",
+    variant: "dark",
+    size: "md",
+    icon: <PlusIcon />,
   },
 };
 
 export const Outline: Story = {
   args: {
-    children: "Outline Button",
+    children: "Cancel",
     variant: "outline",
     size: "md",
-    colorscheme: "primary",
+    icon: <PlusIcon />,
   },
+};
+
+export const Ghost: Story = {
+  args: {
+    children: "Ghost Button",
+    variant: "ghost",
+    size: "md",
+    icon: <PlusIcon />,
+  },
+};
+
+// Size variations
+export const Small: Story = {
+  args: {
+    children: "Small Button",
+    variant: "primary",
+    size: "sm",
+    icon: <PlusIcon />,
+  },
+};
+
+export const Large: Story = {
+  args: {
+    children: "Large Button",
+    variant: "primary",
+    size: "lg",
+    icon: <PlusIcon />,
+  },
+};
+
+// Icon position variations
+export const IconRight: Story = {
+  args: {
+    children: "Icon Right",
+    variant: "primary",
+    size: "md",
+    icon: <PlusIcon />,
+    iconPosition: "right",
+  },
+};
+
+export const NoIcon: Story = {
+  args: {
+    children: "No Icon",
+    variant: "primary",
+    size: "md",
+  },
+};
+
+// Disabled state
+export const Disabled: Story = {
+  args: {
+    children: "Disabled Button",
+    variant: "primary",
+    size: "md",
+    icon: <PlusIcon />,
+    disabled: true,
+  },
+};
+
+// All variants showcase
+export const AllVariants: Story = {
+  render: () => (
+    <div className="flex flex-wrap gap-4">
+      <Button variant="primary" icon={<PlusIcon />}>
+        Primary
+      </Button>
+      <Button variant="secondary" icon={<PlusIcon />}>
+        Secondary
+      </Button>
+      <Button variant="danger" icon={<PlusIcon />}>
+        Danger
+      </Button>
+      <Button variant="success" icon={<PlusIcon />}>
+        Success
+      </Button>
+      <Button variant="warning" icon={<PlusIcon />}>
+        Warning
+      </Button>
+      <Button variant="dark" icon={<PlusIcon />}>
+        Dark
+      </Button>
+      <Button variant="outline" icon={<PlusIcon />}>
+        Outline
+      </Button>
+      <Button variant="ghost" icon={<PlusIcon />}>
+        Ghost
+      </Button>
+    </div>
+  ),
 };
--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -1,64 +1,92 @@
 import { cva, type VariantProps } from "class-variance-authority";
-import { forwardRef, type ComponentProps } from "react";
+import { forwardRef, type ComponentProps, type ReactNode } from "react";
 import { cn } from "../../../utils";
 
 const buttonStyles = cva(
   [
-    "w-full",
-    "rounded-md",
-    "px-4",
-    "py-2",
+    "inline-flex",
+    "items-center",
+    "justify-center",
+    "gap-2",
+    "rounded-full",
+    "px-6",
+    "py-3",
     "text-sm",
     "font-medium",
+    "transition-colors",
+    "focus-visible:outline-none",
+    "focus-visible:ring-2",
+    "focus-visible:ring-offset-2",
     "disabled:cursor-not-allowed",
-    "text-left",
+    "disabled:opacity-50",
   ],
   {
     variants: {
       variant: {
-        solid: "bg-blue-500 text-white hover:bg-blue-600",
-        outline: "border border-blue-500 text-blue-500 hover:bg-blue-50",
+        primary:
+          "bg-blue-500 text-white hover:bg-blue-600 focus-visible:ring-blue-500",
+        secondary:
+          "bg-gray-200 text-gray-900 hover:bg-gray-300 focus-visible:ring-gray-500",
+        danger:
+          "bg-red-500 text-white hover:bg-red-600 focus-visible:ring-red-500",
+        success:
+          "bg-green-500 text-white hover:bg-green-600 focus-visible:ring-green-500",
+        warning:
+          "bg-yellow-400 text-gray-900 hover:bg-yellow-500 focus-visible:ring-yellow-500",
+        dark: "bg-gray-800 text-white hover:bg-gray-900 focus-visible:ring-gray-500",
+        outline:
+          "border border-gray-300 bg-transparent text-gray-700 hover:bg-gray-50 focus-visible:ring-gray-500",
+        ghost:
+          "bg-transparent text-gray-700 hover:bg-gray-100 focus-visible:ring-gray-500",
       },
       size: {
-        sm: "px-4 py-2 text-sm",
-        md: "px-6 py-3 text-base",
-        lg: "px-8 py-4 text-lg",
-      },
-      colorscheme: {
-        primary: "text-white underline",
+        sm: "px-4 py-2 text-xs",
+        md: "px-6 py-3 text-sm",
+        lg: "px-8 py-4 text-base",
       },
     },
-    compoundVariants: [
-      {
-        variant: "solid",
-        colorscheme: "primary",
-        class: "bg-blue-500 text-white hover:bg-blue-600",
-      },
-      {
-        variant: "outline",
-        colorscheme: "primary",
-        class: "border border-blue-500 text-blue-500 hover:bg-blue-50",
-      },
-    ],
     defaultVariants: {
-      variant: "solid",
+      variant: "primary",
       size: "md",
-      colorscheme: "primary",
     },
   }
 );
 
 export type ButtonProps = ComponentProps<"button"> &
-  VariantProps<typeof buttonStyles>;
+  VariantProps<typeof buttonStyles> & {
+    icon?: ReactNode;
+    iconPosition?: "left" | "right";
+  };
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ variant, size, colorscheme, className, ...props }, ref) => {
+  (
+    {
+      variant,
+      size,
+      className,
+      children,
+      icon,
+      iconPosition = "left",
+      ...props
+    },
+    ref
+  ) => {
     return (
       <button
         ref={ref}
-        className={cn(buttonStyles({ variant, size, colorscheme, className }))}
+        className={cn(buttonStyles({ variant, size, className }))}
         {...props}
-      />
+      >
+        {icon && iconPosition === "left" && (
+          <span className="flex-shrink-0">{icon}</span>
+        )}
+        {children}
+        {icon && iconPosition === "right" && (
+          <span className="flex-shrink-0">{icon}</span>
+        )}
+      </button>
     );
   }
 );
+
+Button.displayName = "Button";
